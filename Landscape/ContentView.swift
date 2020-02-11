bb077465ac8cb39f2717a4ae21e97f00a6622934
Vivek Selvaraj <vivek.selvaraj@icloud.com>
2020-02-11 21:00:35 +0530

    Initial Commit

---
+++ Landscape/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landscape
+//
+//  Created by Vivek Selvaraj on 11/02/20.
+//  Copyright © 2020 Vivek Selvaraj. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
